2324e809b626fdd583c4efdac6f26b2b95d61428
ktejas <krishnasalimeda@gmail.com>
2019-04-22 07:27:04 +0530

    Updated PlayerController

--- Assets/Scripts/PeopleSpawner.cs
+++ Assets/Scripts/PeopleSpawner.cs
@@ -1,26 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PeopleSpawner : MonoBehaviour
 {
     public static int numOfPeopleToSpawn = 8;
     public GameObject person;
+    public GameObject uiPeopleToPass;
 
     private GameObject[] people = new GameObject[numOfPeopleToSpawn];
+    private int personId = 0;
+    BoxCollider2D startCollider;
 
     void Start()
     {
-        for(int i=0; i<numOfPeopleToSpawn; i++)
+        //TODO: uiPeopleToPass.GetComponent<>
+        startCollider = GameObject.FindGameObjectWithTag("Start").GetComponent<BoxCollider2D>();
+        for (int i=0; i<numOfPeopleToSpawn; i++)
         {
             people[i] = person;
-            GameObject.Instantiate(person);
-            person.SetActive(true);
+            GameObject.Instantiate(people[i]);
+            people[i].SetActive(true);
         }
+
+        StartCoroutine(ActivatePeople(1.0f));
     }
 
-    void Update()
+    IEnumerator ActivatePeople(float i_startTime)
     {
-        
+        people[personId].SetActive(true);
+        if(numOfPeopleToSpawn < 7)
+        {
+            personId++;
+            StartCoroutine(ActivatePeople(Random.Range(1.0f, 4.0f)));
+        }
+
+        yield return new WaitForSeconds(i_startTime);
     }
 }
